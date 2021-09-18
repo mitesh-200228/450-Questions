5e3d9d088c75ead22a438e737c40c68c3eaf2f8e
Mitesh <miteshbediya1999@gmail.com>
2021-09-18 12:23:23 +0530

    update

---
+++ Array36.cpp
@@ -0,0 +1,86 @@
+#include<unordered_set>
+#include<stack>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<climits>
+#include<cmath>
+#include<math.h>
+#include<limits>
+ 
+#define ll long long int
+const ll N = 1e5+ 2,MOD = 1e9+7;
+using namespace std;
+ 
+// best for finding power of a^b in O(logn)
+int power(ll a,ll b) {
+    ll ans = 1;
+    while(b>0){
+        if(b%2){
+            ans=ans*a;
+        }
+        a=a*a;b>>=1;
+    }
+    return ans;
+}
+ 
+ll gcd(ll a, string b){
+    ll res = 0;
+    for (int i = 0; i < b.length(); i++){
+        res = ((res * 10) + (b[i] - '0')) % a;
+    }
+    return __gcd(a,res);
+}
+bool numberChecker(int x){
+    string s = to_string(x);
+    int len = s.length();
+    // cout<<len<<endl;
+    for(ll i=0; i<len/2 ;i++){
+        if(s[i]!=s[len-i-1]){
+            return false;
+        }
+    }
+
+    return true;
+}
+void solve(){
+    ll n;cin>>n;
+    vector<ll> v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int j=v.size()-1;
+    int i=0;
+    bool flag = true;
+    while(i<=j){
+        if(!numberChecker(v[i])){
+            cout<<"0"<<endl;
+            flag = false;
+            break;
+        }
+        i++;
+    }
+    if(flag) cout<<"1"<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
